beb4c134fa5e2284602e3899cf3fc5735815c5d5
mariomorvan <mario.morvan.18@ucl.ac.uk>
2021-04-12 09:46:59 +0100

    remove compressed files

--- download_data.sh
+++ download_data.sh
@@ -10,3 +10,8 @@ mkdir data/noisy_train
 tar -xvf ml_data_challenge_database/params_train.tar -C data/params_train/
 tar -xvf ml_data_challenge_database/noisy_test.tar -C data/noisy_test/
 tar -xvf ml_data_challenge_database/noisy_train.tar -C data/noisy_train/
+
+rm ml_data_challenge_database.zip
+rm ml_data_challenge_database/params_train.tar
+rm ml_data_challenge_database/noisy_test.tar
+rm ml_data_challenge_database/noisy_train.tar 
